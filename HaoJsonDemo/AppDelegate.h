6f13eef1611b8e76ff3e783a78ceaa9331c0b458
BabyShung <hao-zheng@uiowa.edu>
2014-04-12 16:36:23 -0500

    Initial Commit

---
+++ HaoJsonDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HaoJsonDemo
+//
+//  Created by Hao Zheng on 4/12/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HaoJsonDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HaoJsonDemo
+//
+//  Created by Hao Zheng on 4/12/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
