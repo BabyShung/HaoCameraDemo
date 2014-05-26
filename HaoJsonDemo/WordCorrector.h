749eb32b26126f7cb1a41addbc525eda626ce6cf
Charlie Gao <gcte2010@gmail.com>
2014-05-26 16:15:55 -0500

    add word correction class but not implement

---
+++ HaoJsonDemo/WordCorrector.h
@@ -0,0 +1,15 @@
+//
+//  WordCorrector.h
+//  EdibleCameraApp
+//
+//  Created by CharlieGao on 5/26/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WordCorrector : NSObject
+
+-(NSString*)correct:(NSString*)inputText;
+
+@end
