a866b3c76e162584f4c6729c977c01811345e193
Charlie Gao <gcte2010@gmail.com>
2014-05-30 14:07:20 -0500

    Revert 14d55c8..940e152
    This rolls back to commit 14d55c824308785146ae6f20805e64837a110141.

--- HaoJsonDemo/ImagePreProcessor.h
+++ HaoJsonDemo/ImagePreProcessor.h
@@ -21,6 +21,8 @@ typedef struct{
 
 -(cv::Mat)toGrayMat:(UIImage *) inputImage;
 
+-(cv::Mat)threadholdControl:(cv::Mat) inputImage;
+
 -(cv::Mat)gaussianBlur:(cv::Mat)inputImage :(int)h :(int)w; // size.height size.weight
 
 -(cv::Mat)laplacian:(cv::Mat) inputImage;
