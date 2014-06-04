5b6f6fa7af78629fe23621ff12c6b19d0ff64ecd
BabyShung <hao-zheng@uiowa.edu>
2014-06-03 21:31:13 -0500

    Stable version and added comments

--- HaoJsonDemo/CameraManager.h
+++ HaoJsonDemo/CameraManager.h
@@ -12,6 +12,7 @@
 @protocol CameraManageCDelegate
 
 @required
+//this is because capturing photo is async, when it is done, tell delegate it is done
 - (void) imageDidCaptured:(UIImage *) image;
 @end
 
@@ -20,12 +21,14 @@
 
 @property (retain, nonatomic) id <CameraManageCDelegate> imageDelegate;
 
-
+//switch camera
 -(void)switchCamera;
 
+//session
 - (void) startRunning;
 - (void) stopRunning;
 
+//torch
 -(void)turnOffTorch:(UIButton *)btn;
 -(BOOL) torchAvailable;
 -(BOOL) torchActive;
@@ -34,10 +37,14 @@
 - (void) torchBtnPressed:(UIButton *)btn;
 
 -(AVCaptureVideoPreviewLayer *)createPreviewLayer;
+
+//capture photo
 - (void)capturePhoto:(UIInterfaceOrientation)orientation;
 
+//focus a point
 -(void)focus:(CGPoint)aPoint andFocusView:(UIView *)view;
 
+//clean up, be careful
 -(void)clearResource;
 
 @end
