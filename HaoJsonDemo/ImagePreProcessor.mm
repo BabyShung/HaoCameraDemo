4359b0a52f49c4bdf05cb006b670a4f29ef0cc2f
Charlie Gao <gcte2010@gmail.com>
2014-07-19 21:30:01 -0500

    charlie minor change

--- HaoJsonDemo/ImagePreProcessor.mm
+++ HaoJsonDemo/ImagePreProcessor.mm
@@ -354,15 +354,15 @@ using namespace std;
                 counter_outer++;
                 
             }
-            else{
+            
                 sum_pixl = sum_pixl + pixl_int;
-            }
+            
             
             
         }
     }
     //count the average of the pixels
-    int ave_pixl = sum_pixl/(rows*cols-counter_outer);
+    int ave_pixl = sum_pixl/(rows*cols);
     int ave_outer_pixl = sum_outer_pixl/counter_outer;
     NSLog(@"ImagePrePro: all: %d",ave_pixl);
     NSLog(@"ImagePrePro: out: %d",ave_outer_pixl);
